ef8d13936aaabbf22e53ed21c5af7f99b607ccbd
faisal-saddique <faisalsaddique7964@gmail.com>
2023-08-11 13:58:17 +0500

    adde supoort for updating the vectorstore

--- utilities/create_vectorstore.py
+++ utilities/create_vectorstore.py
@@ -1,6 +1,15 @@
 # Import required libraries
-from langchain.document_loaders import (PyPDFLoader, DirectoryLoader, TextLoader)
-from langchain.document_loaders.csv_loader import CSVLoader
+# Import required libraries
+from langchain.document_loaders import (  
+    PyPDFLoader, # For loading PDF files
+    DirectoryLoader, # For loading files from a directory 
+    TextLoader, # For loading plain text files
+    Docx2txtLoader, # For loading DOCX files
+    UnstructuredPowerPointLoader, # For loading PPTX files 
+    UnstructuredExcelLoader # For loading XLSX files
+)
+from langchain.document_loaders.csv_loader import CSVLoader # For loading CSV files  
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -22,44 +31,61 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 data_directory = "data"
 
 # Load your documents from different sources
+
+
 def get_documents():
-    # Create loaders for PDF, text, and CSV files in the specified directory
-    pdf_loader = DirectoryLoader(f"./{data_directory}", glob="**/*.pdf", loader_cls=PyPDFLoader)
-    txt_loader = DirectoryLoader(f"./{data_directory}", glob="**/*.txt", loader_cls=TextLoader)
-    csv_loader = DirectoryLoader(f"./{data_directory}", glob="**/*.csv", loader_cls=CSVLoader)
+
+    # Create loaders for PDF, text, CSV, DOCX, PPTX, XLSX files in the specified directory
+    pdf_loader = DirectoryLoader(
+        f"./{data_directory}", glob="**/*.pdf", loader_cls=PyPDFLoader)
+    txt_loader = DirectoryLoader(
+        f"./{data_directory}", glob="**/*.txt", loader_cls=TextLoader)
+    csv_loader = DirectoryLoader(
+        f"./{data_directory}", glob="**/*.csv", loader_cls=CSVLoader)
+    docx_loader = DirectoryLoader(
+        f"./{data_directory}", glob="**/*.docx", loader_cls=Docx2txtLoader)
+    pptx_loader = DirectoryLoader(
+        f"./{data_directory}", glob="**/*.pptx", loader_cls=UnstructuredPowerPointLoader)
+    xlsx_loader = DirectoryLoader(
+        f"./{data_directory}", glob="**/*.xlsx", loader_cls=UnstructuredExcelLoader)
 
     # Initialize documents variable
     docs = None
 
-    # Load PDF, text, and CSV files using the respective loaders
-    pdf_data = pdf_loader.load()  # Load PDF files
-    txt_data = txt_loader.load()  # Load text files
-    csv_data = csv_loader.load()  # Load CSV files
+    # Load files using the respective loaders
+    pdf_data = pdf_loader.load()
+    txt_data = txt_loader.load()
+    csv_data = csv_loader.load()
+    docx_data = docx_loader.load()
+    pptx_data = pptx_loader.load()
+    xlsx_data = xlsx_loader.load()
 
     # Combine all loaded data into a single list
-    docs = pdf_data + txt_data + csv_data
+    docs = pdf_data + txt_data + csv_data + docx_data + pptx_data + xlsx_data
 
     # Return all loaded data
     return docs
+
 
 # Get the raw documents from different sources
 raw_docs = get_documents()
 
 # Split the documents into smaller chunks
 text_splitter = RecursiveCharacterTextSplitter(
-    separators=["\n","\n\n"],
-    chunk_size=1500, 
+    separators=["\n", "\n\n"],
+    chunk_size=1500,
     chunk_overlap=20
-    )
+)
 
 docs = text_splitter.split_documents(raw_docs)
 
 # for doc in docs:
-    # print(doc)
+# print(doc)
 
 # Print the number of documents and characters in the first document
 print(f'You have {len(docs)} document(s) in your data')
-print(f'There are {len(docs[0].page_content)} characters in your first document')
+print(
+    f'There are {len(docs[0].page_content)} characters in your first document')
 
 # Create OpenAIEmbeddings object using the provided API key
 embeddings = OpenAIEmbeddings()
@@ -67,27 +93,13 @@ embeddings = OpenAIEmbeddings()
 # Create FAISS vector store from the documents and embeddings
 db = FAISS.from_documents(docs, embeddings)
 
-# Uncomment these lines if you want to store the Vector Database locally for future use.
-# You could load it up anytime you want.
-# Save the vector database locally in a directory named "faiss_index"
-db.save_local("louisiana_nursery_chatbot_vectorstore")
-
-""" 
-# Create OpenAI language model
-llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
-
-# Create question answering chain using the OpenAI language model
-chain = load_qa_chain(llm, chain_type="stuff")
-
-# Start the interactive loop to take user queries
-while True:
-    query = input("Ask your query:")  # Take user input
-
-    # Perform similarity search in the vector database and get the most similar documents
-    docs = db.similarity_search(query, k=3)
-
-    # Run the question answering chain on the selected documents and query
-    response = chain.run(input_documents=docs, question=query)
-
-    # Print the response from the question answering chain
-    print(f"Response: {response}") """
+# Provide the embeddings instance to associate embeddings with the vector store
+try:
+    print("Found old vector store, updating it with the new information..")
+    old_vectorstore = FAISS.load_local(
+        "louisiana_nursery_chatbot_vectorstore", embeddings)
+    db.merge_from(old_vectorstore)
+    db.save_local("louisiana_nursery_chatbot_vectorstore")
+except:
+    print(f"No existing vectorstore found. Creating a new one.")
+    db.save_local("louisiana_nursery_chatbot_vectorstore")
